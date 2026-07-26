f580f129acaba0bac4251666771b2c4b163093ab
SINGH0883 <yuvraj0883@gmail.com>
2026-07-26 13:36:03 +0530

    Fix app startup crash by defining reactNativeHost in MainApplication.kt and update app favicon/icons to icon128.png

--- transnova-mobile/android/app/src/main/java/com/singh0883/transnovamobile/MainApplication.kt
+++ transnova-mobile/android/app/src/main/java/com/singh0883/transnovamobile/MainApplication.kt
@@ -5,27 +5,41 @@ import android.content.res.Configuration
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ExpoReactHostFactory
+import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    ExpoReactHostFactory.getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.toMutableList().apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          add(KeyboardPackage())
+  override val reactNativeHost: ReactNativeHost =
+    ReactNativeHostWrapper(
+      this,
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> {
+          val packages = PackageList(this).packages.toMutableList()
+          packages.add(KeyboardPackage())
+          return packages
         }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
     )
-  }
+
+  override val reactHost: ReactHost
+    get() = ExpoReactHostFactory.getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
